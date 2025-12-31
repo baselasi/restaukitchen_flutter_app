852e9efb264a1234dbf9ffe4fbe8139d4143eedc
bassel <bassel-assi@live.com>
2025-12-31 15:15:44 +0100

    feat: navigation to page

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -3,10 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:restaukitchen_app/page/homePage/bloc/orders_count_cubit.dart';
 import 'package:restaukitchen_app/page/homePage/bloc/table_count_cubit.dart';
 import 'package:restaukitchen_app/page/homePage/home_page.dart';
+import 'package:restaukitchen_app/page/menusPage/menus_page.dart';
 
 enum Pages {
   home,
-  // menus,
+  menus,
   // tables,
   // settings,
 }
@@ -23,6 +24,8 @@ Widget buildPage(Pages page) {
         ],
         child: HomePage(),
       );
+    case Pages.menus:
+      return MenusPage();
   }
 }
 
@@ -30,5 +33,7 @@ Widget getPageTitle(Pages page) {
   switch (page) {
     case Pages.home:
       return const Text('Home Page');
+    case Pages.menus:
+      return const Text('Menus Page');
   }
 }
---
+++ lib/page/menusPage/menus_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MenusPage extends StatefulWidget {
+  const MenusPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MenusPageState();
+  }
+}
+
+class _MenusPageState extends State<MenusPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
